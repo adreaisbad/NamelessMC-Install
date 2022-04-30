535e94ef559b580534d1d5a7bd8bd45530ad2f3c
adrea <90507395+adreaisbad@users.noreply.github.com>
2022-04-30 08:55:07 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@
 # Everyone is permitted to copy and distribute verbatim or modified
 # copies of this license document, and changing it is allowed as long
 # as the name is changed.
-# 
+#
 #           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
 #  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-# 
+#
 # 0. You just DO WHAT THE FUCK YOU WANT TO.
 ##############################################################################
 
-curl -s -L https://github.com/DxqtxD/NamelessMC-Install/blob/main/update.sh | bash
+curl -s -L https://github.com/adreaisbad/NamelessMC-Install/blob/main/update.sh | bash
